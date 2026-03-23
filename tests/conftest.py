aa0fb7938ca497bb684a8994883558c5f41cd016
Kirill <kbelous@gmail.com>
2026-03-23 19:31:01 +0300

    fix: async test problem

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 from httpx import AsyncClient
 from app.main import app
 
-@pytest.fixture(scope="function")
+@pytest.fixture
 async def client():
     async with AsyncClient(app=app, base_url="http://test") as ac:
         yield ac
