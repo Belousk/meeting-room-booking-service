19be7383f6b411208a88c71515b3ee50d4a0b728
Kirill <kbelous@gmail.com>
2026-03-23 19:22:09 +0300

    fix: explicit indicate that fixture is generated for each test

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 from httpx import AsyncClient
 from app.main import app
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 async def client():
     async with AsyncClient(app=app, base_url="http://test") as ac:
         yield ac
